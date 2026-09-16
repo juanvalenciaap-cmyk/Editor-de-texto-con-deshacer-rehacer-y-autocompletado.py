32de7f9524ecbb1b99c5092a773a6e8dafc6f378
Juan Grajales <j45347664@gmail.com>
2026-09-15 21:29:09 -0500

    no se escribir( solo cambie una cosa de ortografia :D)

--- posicion_cursor.py
+++ posicion_cursor.py
@@ -1,5 +1,5 @@
-# por decision creativa, esat cosa solo va a tener la logica para mover el cursor, nada mas 
-# despues con más tiempo y en otro archivo mas bonito dond eya va a ir la logica del editor
+# por decision creativa, esta cosa solo va a tener la logica para mover el cursor, nada mas 
+# despues con más tiempo y en otro archivo mas bonito donde ya va a ir la logica del editor
 # se mete la logica para verificar, donde porongas estamos y si es valido hacer X movimiento
 class PosicionCursor:
     def __init__(self, fila=0, columna=0):
